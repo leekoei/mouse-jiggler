9335a02b87d818df48ab5a92d415104381e43549
Ron Li <leekoei@gmail.com>
2019-08-16 20:23:37 -0400

    Initial commit.

---
+++ MouseJiggler.ino
@@ -0,0 +1,43 @@
+/**
+ * Mouse Jiggler
+ * 
+ * This Arduino script turns USB-ready board into 'Mouse Jiggler'. 
+ * It emulate a mouse and move the cursor in cerntain pattern. 
+ * By keeping mouse moving, computer won't get into sleep mode or turn off.
+ * 
+ * All rights reserved.
+ * 
+ * Xiangrong Li
+ * 
+ * 2019.08
+ * 
+ */
+#include "Mouse.h"
+
+/* Const definition */
+const int move_rect = 0;
+
+/* Global variable definitions */
+int rect_len      = 20;
+int move_delay    = 50;
+int move_pattern  = move_rect;
+
+
+void setup() {
+  /* Arduino takes control of the mouse */
+  Mouse.begin();
+}
+
+void loop() {
+  /* Rectangle movement */
+  if (move_pattern == move_rect) {
+    Mouse.move(0, rect_len, 0);
+    delay(move_delay);
+    Mouse.move(rect_len, 0, 0);
+    delay(move_delay);
+    Mouse.move(0, -rect_len, 0);
+    delay(move_delay);
+    Mouse.move(-rect_len, 0, 0);
+    delay(move_delay);
+  }
+}
