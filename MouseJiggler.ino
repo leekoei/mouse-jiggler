fca0e68d4e983af7e7490263891cfc55e9fa697d
Ron Li <leekoei@gmail.com>
2019-08-16 21:03:16 -0400

    Add circle movement.

--- MouseJiggler.ino
+++ MouseJiggler.ino
@@ -24,11 +24,14 @@
 
 /* Const definition */
 const int move_rect = 0;
+const int move_circle = 1;
+
+const int circle_approx_n = 50;
 
 /* Global variable definitions */
-int rect_len      = 20;
-int move_delay    = 50;
-int move_pattern  = move_rect;
+int rect_len      = 15;
+int move_delay    = 5;
+int move_pattern  = move_circle;
 
 
 void setup() {
@@ -47,5 +50,13 @@ void loop() {
     delay(move_delay);
     Mouse.move(-rect_len, 0, 0);
     delay(move_delay);
+  }
+  if (move_pattern == move_circle) {
+    for(int i = 0; i < circle_approx_n; i++) {
+      float x = rect_len * cos(2*3.14*(i + 1)/circle_approx_n);
+      float y = rect_len * sin(2*3.14*(i + 1)/circle_approx_n);
+      Mouse.move(x, y, 0);
+      delay(move_delay);
+    }
   }
 }
